be60e473fa9b188d7c4c4e3d7e0ea6f6d9daf6b1
Vishal586 <128839643+Vishal586@users.noreply.github.com>
2025-02-05 11:43:54 +0530

    Add files via upload

---
+++ my-app/src/utils/TechstackList.js
@@ -0,0 +1,88 @@
+import { BsFiletypeHtml, BsFiletypeCss, BsBootstrap } from "react-icons/bs";
+import { FaNodeJs } from "react-icons/fa";
+import { TbBrandNextjs } from "react-icons/tb";
+import { FaJava } from "react-icons/fa";
+import { SiSpringboot } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import {
+    SiJavascript,
+    SiReact,
+    SiTailwindcss,
+    SiMongodb,
+    SiMysql,
+    SiGithub,
+    SiExpress,
+  } from "react-icons/si";
+
+export const TechstackList = [
+    {
+        _id: 1,
+        name: "HTML",
+        icon: BsFiletypeHtml,
+    },
+    {
+        _id: 2,
+        name: "CSS",
+        icon: BsFiletypeCss,
+      },
+      {
+        _id: 3,
+        name: "Tailwind",
+        icon: SiTailwindcss,
+      },
+      {
+        _id: 4,
+        name: "Javascript",
+        icon: SiJavascript,
+      },
+      {
+        _id: 5,
+        name: "React JS",
+        icon: SiReact,
+      },
+      {
+        _id: 6,
+        name: "Nextjs",
+        icon: TbBrandNextjs,
+      },
+      {
+        _id: 7,
+        name: "Node JS ",
+        icon: FaNodeJs,
+      },
+      {
+        _id: 8,
+        name: "Express JS ",
+        icon: SiExpress,
+      },
+      {
+        _id: 9,
+        name: "Mongodb ",
+        icon: SiMongodb,
+      },
+      {
+        _id: 10,
+        name: "SQL ",
+        icon: SiMysql,
+      },
+      {
+        _id: 11,
+        name: "Git / Github ",
+        icon: SiGithub,
+      },
+      {
+        _id: 12,
+        name: "Java",
+        icon: FaJava,
+      },
+      {
+        _id: 13,
+        name: "Springboot",
+        icon: SiSpringboot,
+      },
+      {
+        _id: 14,
+        name: "Docker",
+        icon: FaDocker,
+      },
+];
